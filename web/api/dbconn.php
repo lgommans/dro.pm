50dba19fc3a6d0aee08a3edaeb88719f790bcb23
Luc Gommans <git-client@lgms.nl>
2019-12-02 23:25:55 +0100

    update dbconn.php (new config variable there)

--- web/api/dbconn.php
+++ web/api/dbconn.php
@@ -2,8 +2,6 @@
 	$db = @new mysqli('p:localhost', 'dropm', password_changeme, 'dropm');
 	if ($db->connect_error)
 		die('Database connection error');
-	
-	define('LENGTH_SHORT', 1);
-	define('LENGTH_MEDIUM', 2);
-	define('LENGTH_LONG', 3);
-	define('BUGEMAILADDR', email_changeme);
+
+	$uploaddir = 'uploads';
+
